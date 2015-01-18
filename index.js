860485c78532ee250714f2f618fde72b66eb3234
RangerMauve <RangerMauve@htomail.com>
2015-01-18 00:24:53 -0500

    Defer making the patcher if no initial value set

--- index.js
+++ index.js
@@ -4,13 +4,24 @@ var patcher = require("html-patcher");
 module.exports = patcherStream;
 
 function patcherStream(element, initial) {
-	var patch = patcher(element, initial || "");
-
 	var stream = new Writable();
-	stream._write = function (chunk, encoding, cb) {
-		patch(chunk.toString());
+	var patch;
+	
+	if (initial) init(initial);
+
+	stream._write = function(chunk, encoding, cb) {
+		var html = chunk.toString();
+		if (!patch) {
+			init(html);
+		} else {
+			patch(html);
+		}
 		cb(null);
 	}
 
 	return stream;
+
+	function init(initial) {
+		patch = patcher(element, initial || "<div></div>");
+	}
 }
